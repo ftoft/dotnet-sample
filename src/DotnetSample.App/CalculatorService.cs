c46b6d47e3abe915615a6e1caf838546846c4166
Finn Skødt Toft <fito@cloudfactory.dk>
2023-04-28 12:27:14 +0200

    Initial Project

---
+++ src/DotnetSample.App/CalculatorService.cs
@@ -0,0 +1,26 @@
+﻿namespace DotnetSample.App;
+
+public class CalculatorService
+{
+    private double _currentValue;
+
+    public void Add(double operand)
+    {
+        _currentValue += operand;
+    }
+
+    public void Subtract(double operand)
+    {
+        _currentValue -= operand;
+    }
+
+    public double Result()
+    {
+        return _currentValue;
+    }
+
+    public void Clear()
+    {
+        _currentValue = 0;
+    }
+}
---
+++ src/DotnetSample.Test/UnitTest1.cs
@@ -0,0 +1,65 @@
+using DotnetSample.App;
+
+namespace DotnetSample.Test;
+
+public class CalculatorServiceTests
+{
+    private readonly CalculatorService _sut;
+
+    public CalculatorServiceTests()
+    {
+        _sut = new CalculatorService();
+    }
+
+    [Fact]
+    public void Add_AddsOperandToCurrentValue()
+    {
+        // Arrange
+        _sut.Add(5);
+
+        // Act
+        _sut.Add(10);
+
+        // Assert
+        Assert.Equal(15, _sut.Result());
+    }
+
+    [Fact]
+    public void Subtract_SubtractsOperandFromCurrentValue()
+    {
+        // Arrange
+        _sut.Add(10);
+
+        // Act
+        _sut.Subtract(5);
+
+        // Assert
+        Assert.Equal(5, _sut.Result());
+    }
+
+    [Fact]
+    public void Result_ReturnsCurrentValue()
+    {
+        // Arrange
+        _sut.Add(10);
+
+        // Act
+        var result = _sut.Result();
+
+        // Assert
+        Assert.Equal(10, result);
+    }
+
+    [Fact]
+    public void Clear_ResetsCurrentValueToZero()
+    {
+        // Arrange
+        _sut.Add(10);
+
+        // Act
+        _sut.Clear();
+
+        // Assert
+        Assert.Equal(0, _sut.Result());
+    }
+}
